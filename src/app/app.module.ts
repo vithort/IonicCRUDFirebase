afca952ba36a3650a35d3b8b292f540427b1efdc
vithort <vithort@gmail.com>
2018-10-30 19:23:16 -0300

    Instalação de Componentes
    Criar Provider:
    > ionic g provider contact
    
    Criar Página:
    > ionic g page contact-edit
    
    Instalar AngularFire2 e Firebase:
    > npm install firebase angularfire2 --save
    
    Importar Dependencias;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,12 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ContactProvider } from '../providers/contact/contact';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,16 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyCaMOgM4LouAL5CZuw2FXR5Ka_M74k6nLw",
+      authDomain: "contatos-2675b.firebaseapp.com",
+      databaseURL: "https://contatos-2675b.firebaseio.com",
+      projectId: "contatos-2675b",
+      storageBucket: "contatos-2675b.appspot.com",
+      messagingSenderId: "1056652219513"
+    }),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +37,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ContactProvider
   ]
 })
 export class AppModule {}
