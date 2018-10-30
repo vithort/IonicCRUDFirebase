994f7414a739bdbfeea9fb926d4ced327c24dd8f
vithort <vithort@gmail.com>
2018-10-30 19:47:11 -0300

    Código e Erros + Soluções
    Erro: object(...) is not a function
    Solução:
    > npm uninstall firebase --save
    > npm uninstall angularfire2 --save
    > npm i firebase@4.6.0 angularfire2@5.0.0-rc.3 --save
    
    Erro: Uncaught Error: permission_denied at /contacts: Client doesn't have permission to access the desired data.﻿
    Solução: Vá em Firebase > Realtime Database > Rules e mude para "True" a permissão de leitura e escrita!

--- src/pages/contact-edit/contact-edit.ts
+++ src/pages/contact-edit/contact-edit.ts
@@ -1,12 +1,7 @@
+import { ContactProvider } from './../../providers/contact/contact';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the ContactEditPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -14,12 +9,58 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'contact-edit.html',
 })
 export class ContactEditPage {
+  title: string;
+  form: FormGroup;
+  contact: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, public navParams: NavParams,
+    private formBuilder: FormBuilder, private provider: ContactProvider,
+    private toast: ToastController) {
+
+    // maneira 1
+    this.contact = this.navParams.data.contact || { };
+    this.createForm();
+
+    // // maneira 2
+    // this.contact = { };
+    // this.createForm();
+
+    // if (this.navParams.data.key) {
+    //   const subscribe = this.provider.get(this.navParams.data.key).subscribe((c: any) => {
+    //     subscribe.unsubscribe();
+
+    //     this.contact = c;
+    //     this.createForm();
+    //   })
+    // }
+
+    this.setupPageTitle();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ContactEditPage');
+  private setupPageTitle() {
+    this.title = this.navParams.data.contact ? 'Alterando contato' : 'Novo contato';
   }
 
+  createForm() {
+    this.form = this.formBuilder.group({
+      key: [this.contact.key],
+      name: [this.contact.name, Validators.required],
+      tel: [this.contact.tel, Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.provider.save(this.form.value)
+        .then(() => {
+          this.toast.create({ message: 'Contato salvo com sucesso.', duration: 3000 }).present();
+          this.navCtrl.pop();
+        })
+        .catch((e) => {
+          this.toast.create({ message: 'Erro ao salvar o contato.', duration: 3000 }).present();
+          console.error(e);
+        })
+    }
+  }
 }
--- src/providers/contact/contact.ts
+++ src/providers/contact/contact.ts
@@ -1,17 +1,44 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
 
-/*
-  Generated class for the ContactProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class ContactProvider {
+  private PATH = 'contacts/';
 
-  constructor(public http: HttpClient) {
-    console.log('Hello ContactProvider Provider');
+  constructor(private db: AngularFireDatabase) {
   }
 
+  getAll() {
+    return this.db.list(this.PATH, ref => ref.orderByChild('name'))
+      .snapshotChanges()
+      .map(changes => {
+        return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+      })
+  }
+
+  get(key: string) {
+    return this.db.object(this.PATH + key).snapshotChanges()
+      .map(c => {
+        return { key: c.key, ...c.payload.val() };
+      });
+  }
+
+  save(contact: any) {
+    return new Promise((resolve, reject) => {
+      if (contact.key) {
+        this.db.list(this.PATH)
+          .update(contact.key, { name: contact.name, tel: contact.tel })
+          .then(() => resolve())
+          .catch((e) => reject(e));
+      } else {
+        this.db.list(this.PATH)
+          .push({ name: contact.name, tel: contact.tel })
+          .then(() => resolve());
+      }
+    })
+  }
+
+  remove(key: string) {
+    return this.db.list(this.PATH).remove(key);
+  }
 }
